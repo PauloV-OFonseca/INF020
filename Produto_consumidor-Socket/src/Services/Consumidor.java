0745f637895eca3951dc5a6c774cdc82e1f12fb3
@PauloVitor_OFonseca <paulo@geekbusinesslab.com.br>
2021-12-07 22:01:03 -0300

    consumidor e produtor podem realizar suas funções

--- Produto_consumidor-Socket/src/Services/Consumidor.java
+++ Produto_consumidor-Socket/src/Services/Consumidor.java
@@ -1,5 +1,14 @@
 package Services;
 
 public class Consumidor {
-
+	private Fabrica fabrica;
+	
+	public Consumidor(Fabrica fabrica) {
+		this.fabrica = fabrica;
+	}
+	
+	public void consumir() {
+		fabrica.subtrairContador();
+	}
+	
 }
--- Produto_consumidor-Socket/src/Services/Produtor.java
+++ Produto_consumidor-Socket/src/Services/Produtor.java
@@ -1,5 +1,13 @@
 package Services;
 
 public class Produtor {
-
+	private Fabrica fabrica;
+	
+	public Produtor(Fabrica fabrica) {
+		this.fabrica = fabrica;
+	}
+	
+	public void produzir() {
+		fabrica.somarContador();
+	}
 }
